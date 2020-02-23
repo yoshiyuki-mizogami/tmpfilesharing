a1f2cc4a94af48450fbf7b30d6bf3ddf0a0e77d1
yoshiyuki.mizogami <mizogami00@gmail.com>
2020-02-23 09:25:43 +0900

    implement zip download

--- pages/open/_name.vue
+++ pages/open/_name.vue
@@ -7,6 +7,9 @@
       <v-alert v-if="error" type="error" color="red darken-1">
         {{errorMessage}}
       </v-alert>
+      <v-row justify="center">
+        <v-btn color="primary" @click="downloadAllAsZip">Download all as zip</v-btn>
+      </v-row>
       <v-row v-show="entry.files.length" class="justify-center" max-width="800%" width="80%">
         <v-list max-width="800px" width="100%" >
           <v-list-item-group>
@@ -34,6 +37,7 @@
 <script lang="ts">
 import {Vue, Component} from 'nuxt-property-decorator'
 import axios from 'axios'
+import jsZip from 'jszip'
 interface ReceiveFile{
   name:string,
   size:number,
@@ -69,11 +73,24 @@ export default class EntryItem extends Vue{
     this.entry = data
   }
   async download(file:any){
-    const a = document.createElement('a')
-    a.setAttribute('download', file.name)
-    a.setAttribute('style', 'position:abosolute;right:0;bottom:0;visibility:hidden')
     const url = 'data:octet/stream;base64,' + file.body
     const blob = await fetch(url).then(r=>r.blob())
+    this.saveAs(file.name, blob)
+  }
+  async downloadAllAsZip(){
+    const {files} = this.entry
+    const zip = new jsZip()
+    const folder = zip.folder(this.entry.name)
+    files.forEach(f=>{
+      folder.file(f.name,f.body,{base64:true})
+    })
+    const blob = await zip.generateAsync({type:'blob'})
+    this.saveAs(this.entry.name, blob)
+  }
+  saveAs(filename:string, blob:Blob){
+    const a = document.createElement('a')
+    a.setAttribute('download', filename)
+    a.setAttribute('style', 'position:abosolute;right:0;bottom:0;visibility:hidden')
     a.href = URL.createObjectURL(blob)
     document.body.appendChild(a)
     a.click()
@@ -81,7 +98,6 @@ export default class EntryItem extends Vue{
       URL.revokeObjectURL(a.href)
       document.body.removeChild(a)
     }, 300)
-    
   }
 }
 </script>
